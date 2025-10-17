519a28b084a61b5b30eea3d4288d83d1d14d36fa
Dinesh Kumar S <dineshmsp@outlook.com>
2025-10-17 13:39:38 +0530

    updated fonts and changed products vendor

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -119,7 +119,9 @@ validateNumberInput(event: KeyboardEvent): void {
 
     // Build message grouped by vendor
     Object.keys(vendors).forEach(vendor => {
-      message += `🏷️ [${vendor}] \n`;
+      // Underline vendor name using combining low line, then bold it
+      const underlinedVendor = vendor.split('').map(ch => ch + '\u0332').join('');
+      message += `🏷️ *[${underlinedVendor}]* \n`;
       vendors[vendor].forEach(item => {
       message += `• ${item.name}  -  *${item.quantity}* × ₹${item.value} \n`;
       });
